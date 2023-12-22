d8674f8ba9e55d5719f539faf9cf12173297e909
rocky4546 <24759693+rocky4546@users.noreply.github.com>
2023-12-22 17:45:53 -0600

    Initial release of 0.9.14

--- lib/common/utils.py
+++ lib/common/utils.py
@@ -37,7 +37,7 @@ import tracemalloc
 
 import lib.common.exceptions as exceptions
 
-VERSION = '0.9.14.00-RC13'
+VERSION = '0.9.14.01'
 CABERNET_URL = 'https://github.com/cabernetwork/cabernet'
 CABERNET_ID = 'cabernet'
 CABERNET_REPO = 'manifest.json'
